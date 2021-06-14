b4acc978116729399d38324f91b07b4591762282
Nikolai Schunk <nikolai.schunk@gmail.com>
2021-06-14 14:18:31 +0200

    Add Bootstrap

---
+++ view/includes/head.php
@@ -0,0 +1,8 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Titel der Seite | Nikoden</title>
+
+    <link rel="stylesheet" href="./view/style/bootstrap.min.css">
+    <link rel="stylesheet" href="./view/style/style.css">
+</head>
