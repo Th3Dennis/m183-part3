ec5be25f0d83fd4b43554822bbec196ecb91e738
Nikolai Schunk <nikolai.schunk@gmail.com>
2021-06-14 14:20:20 +0200

    Added Basic Layout

---
+++ Overview.php
@@ -0,0 +1,10 @@
+<html>
+<?php require "./view/includes/head.php" ?>
+
+
+<body>
+<?php require "./view/includes/navbar.php" ?>
+
+</body>
+
+</html>
---
+++ Register.php
@@ -0,0 +1,8 @@
+<html>
+<?php require "./view/includes/head.php" ?>
+
+<body>
+    <?php require "./view/includes/navbar.php" ?>
+</body>
+
+</html>
