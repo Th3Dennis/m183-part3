797f8a45a9a69fd13859a4e94f83a3f5b0ffe229
Nikolai Schunk <nikolai.schunk@gmail.com>
2021-06-28 11:26:08 +0200

    Added Navbar Links

--- view/includes/navbarLogin.php
+++ view/includes/navbarLogin.php
@@ -1,14 +1,14 @@
 <div class="navbarLogin">
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Nikoden</a>
+            <a class="navbar-brand" href="./index.php">Nikoden</a>
             <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="navbar-collapse collapse" id="navbarColor01">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Overview</a>
+                        <a class="nav-link active" aria-current="page" href="./index.php">Overview</a>
                     </li>
                 </ul>
                 <a href="./login.php" class="btn-space-right"><button class="btn btn-primary" type="button">Login</button></a>
