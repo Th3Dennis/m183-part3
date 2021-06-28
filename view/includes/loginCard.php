38e6c98561a2f9c9b5320051473fbae894923ed2
Nikolai Schunk <nikolai.schunk@gmail.com>
2021-06-28 10:32:49 +0200

    Added SignUp & Register Pages with Cards and Forms

---
+++ view/includes/loginCard.php
@@ -0,0 +1,37 @@
+<div class="loginModal">
+    <div id="intro" class="bg-image shadow-2-strong">
+        <div class="mask d-flex align-items-center h-100 card-bg-grey">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-xl-5 col-md-8">
+                        <form class="bg-white  rounded-5 shadow-5-strong p-5">
+                            <legend>Login</legend>
+                            <!-- Email input -->
+                            <div class="form-outline mb-4">
+                                <input type="email" id="form1Example1" class="form-control" />
+                                <label class="form-label" for="form1Example1">Email address</label>
+                            </div>
+
+                            <!-- Password input -->
+                            <div class="form-outline mb-4">
+                                <input type="password" id="Password" class="form-control" />
+                                <label class="form-label" for="Password">Password</label>
+                            </div>
+
+                            <!-- 2 column grid layout for inline styling -->
+                            <div class="row mb-4">
+                                <div class="col">
+                                    <!-- Simple link -->
+                                    <a>Don't have an account yet? <a href="./register.php">Sign Up</a></a>
+                                </div>
+
+                            </div>
+                            <!-- Submit button -->
+                            <button type="submit" class="btn btn-primary btn-block">Login</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ view/includes/registerCard.php
@@ -0,0 +1,42 @@
+<div class="registerModal">
+    <div id="intro" class="bg-image shadow-2-strong">
+        <div class="mask d-flex align-items-center h-100 card-bg-grey" ;">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-xl-5 col-md-8">
+                        <form class="bg-white  rounded-5 shadow-5-strong p-5">
+                            <legend>Register</legend>
+                            <!-- Email input -->
+                            <div class="form-outline mb-4">
+                                <input type="email" id="form1Example1" class="form-control" />
+                                <label class="form-label" for="form1Example1">Email address</label>
+                            </div>
+
+                            <!-- Password input -->
+                            <div class="form-outline mb-4">
+                                <input type="password" id="form1Example2" class="form-control" />
+                                <label class="form-label" for="form1Example2">Password</label>
+                            </div>
+
+                            <!-- Repeat Password input -->
+                            <div class="form-outline mb-4">
+                                <input type="password" id="repeatPassword" class="form-control" />
+                                <label class="form-label" for="repeatPassword">Repeat Password</label>
+                            </div>
+
+                            <!-- 2 column grid layout for inline styling -->
+                            <div class="row mb-4">
+                                <div class="col">
+                                    <!-- Simple link -->
+                                    <a>Already have an account? <a href="./login.php">Login</a></a </div>
+                                </div>
+                            </div>
+                            <!-- Submit button -->
+                            <button type="submit" class="btn btn-primary btn-block">Sign Up</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
