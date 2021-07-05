b6e18b99861878d262ffdf5ca47e78da210bcba2
Th3Dennis <dennis.miceli@hotmail.ch>
2021-07-05 11:17:25 +0200

    Added basic validation for forms

--- view/includes/registerCard.php
+++ view/includes/registerCard.php
@@ -10,19 +10,19 @@
                             <legend>Register</legend>
                             <!-- Email input -->
                             <div class="form-outline mb-4">
-                                <input type="email" id="form1Example1" class="form-control" />
+                                <input type="email" id="form1Example1" class="form-control" required />
                                 <label class="form-label" for="form1Example1">Email address</label>
                             </div>
 
                             <!-- Password input -->
                             <div class="form-outline mb-4">
-                                <input type="password" id="form1Example2" class="form-control" />
+                                <input type="password" id="form1Example2" class="form-control" required/>
                                 <label class="form-label" for="form1Example2">Password</label>
                             </div>
 
                             <!-- Repeat Password input -->
                             <div class="form-outline mb-4">
-                                <input type="password" id="repeatPassword" class="form-control" />
+                                <input type="password" id="repeatPassword" class="form-control" required/>
                                 <label class="form-label" for="repeatPassword">Repeat Password</label>
                             </div>
 
