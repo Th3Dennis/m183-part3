1b85546f81096cf5bff479c3f1b30e1e426f98a7
Th3Dennis <dennis.miceli@hotmail.ch>
2021-07-05 10:52:10 +0200

    Made lite listGroup

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <body>
     <?php require "./view/includes/navbarLogin.php" ?>
     <?php require "./view/includes/footer.php" ?>
+    <?php require "./view/includes/overviewComponent.php" ?>
 
 </body>
 
--- view/includes/listGroup.php
+++ view/includes/listGroup.php
@@ -8,11 +8,19 @@ $allJobs = $jobService->getJobsList();
 
 <div class="list-group">
 
-  <?php foreach($allJobs as $job): ?>
+  <?php foreach ($allJobs as $job) : ?>
 
-  <a href=<?= "/detailPage.php?id=" . $job->id?> class="list-group-item list-group-item-action"><?= $job->name ?></a>
+    <?php if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn']) :   ?>
+
+      <a href=<?= "/detailPage.php?id=" . $job->id ?> class="list-group-item list-group-item-action"><?= $job->name ?></a>
+    
+    <?php else : ?>
+
+      <a href="#" class="list-group-item list-group-item-action " data-bs-toggle="tooltip" data-bs-placement="top" title="You need to login to see details"><?= $job->name ?></a>
 
 
-<?php endforeach; ?>
+    <?php endif; ?>
+
+  <?php endforeach; ?>
 
 </div>
